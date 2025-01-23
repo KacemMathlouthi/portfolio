86bf8c06fb13601e5d5c8d340a0b9c151f873eb1
Kacem Mathlouthi <kacem.mathlouthi@insat.ucar.tn>
2025-01-23 16:25:17 +0100

    Feat: Finished Hackathons sections

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -78,19 +78,19 @@ export const DATA = {
 
   work: [
     {
-      company: "Atomic Finance",
-      href: "https://atomic.finance",
+      company: "Orange",
+      href: "https://www.orangedigitalcenters.com/",
       badges: [],
-      location: "Remote",
-      title: "Bitcoin Protocol Engineer",
-      logoUrl: "/atomic.png",
+      location: "On Site",
+      title: "Machine Learning Engineer Intern",
+      logoUrl: "/orange.jpg",
       start: "May 2021",
       end: "Oct 2022",
       description:
         "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
     },
     {
-      company: "Shopify",
+      company: "LENS",
       badges: [],
       href: "https://shopify.com",
       location: "Remote",
@@ -104,18 +104,10 @@ export const DATA = {
   ],
   education: [
     {
-      school: "Buildspace",
-      href: "https://buildspace.so",
-      degree: "s3, s4, sf1, s5",
-      logoUrl: "/buildspace.jpg",
-      start: "Actively Studying",
-      end: "",
-    },
-    {
       school: "National Institute of Applied Science and Technology",
-      href: "https://uwaterloo.ca",
+      href: "https://insat.rnu.tn/",
       degree: "Bachelor's Degree in Software Engineering",
-      logoUrl: "/waterloo.png",
+      logoUrl: "/insat_logo.png",
       start: "2022",
       end: "2027",
     },
@@ -246,102 +238,77 @@ export const DATA = {
   ],
   hackathons: [
     {
-      title: "Hack Western 5",
-      dates: "November 23rd - 25th, 2018",
-      location: "London, Ontario",
+      title: "1st Place 🥇 Orange x Hexabot Conversational AI Hackathon",
+      dates: "Dec 22, 2024",
+      location: "Hammamet, Tunisia",
       description:
-        "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
+        "Developed plugins and extensions for the Hexabot visual editor, enabling multimodal capabilities such as text-to-speech, speechto-text, and vision integration.",
       image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
-      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
-      links: [],
-    },
-    {
-      title: "DeveloperWeek Hackathon",
-      dates: "February 3rd - 4th, 2018",
-      location: "San Francisco, California",
-      description:
-        "Developed a web application which aggregates social media data regarding cryptocurrencies and predicts future prices.",
-      image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/developer-week.jpg",
-      links: [
-        {
-          title: "Github",
-          icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/cryptotrends/cryptotrends",
-        },
-      ],
-    },
-    {
-      title: "HackDavis",
-      dates: "January 20th - 21st, 2018",
-      location: "Davis, California",
-      description:
-        "Developed a mobile application which allocates a daily carbon emission allowance to users to move towards a sustainable environment.",
-      image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-davis.png",
-      win: "Best Data Hack",
-      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2018/white.svg",
+        "https://avatars.githubusercontent.com/u/31648342?s=280&v=4",
       links: [
         {
           title: "Devpost",
           icon: <Icons.globe className="h-4 w-4" />,
-          href: "https://devpost.com/software/my6footprint",
+          href: "https://devpost.com/software/paranoid-android-submission",
         },
         {
-          title: "ML",
+          title: "Github Repository",
           icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/Wallet6/my6footprint-machine-learning",
-        },
-        {
-          title: "iOS",
-          icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/Wallet6/CarbonWallet",
-        },
-        {
-          title: "Server",
-          icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/Wallet6/wallet6-server",
+          href: "https://github.com/KacemMathlouthi/X2X-Hexabot-Orange-Submission",
         },
       ],
     },
     {
-      title: "ETH Waterloo",
+      title: "2nd Place 🥈 IEEE TSYP 12 CS Technical Challenge",
+      dates: "November 1, 2024 - December 23, 2024",
+      location: "Hammamet, Tunisia",
+      description:
+        "Competed in the Tunisian Students and Young Professionals Computer Science Challenge by creating Smartshield; an AI-powered platform for automated cybersecurity threat detection and response.",
+      image:
+        "https://scontent.ftun14-1.fna.fbcdn.net/v/t39.30808-6/451555034_806009071680012_1549095888316774813_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=UkP4-8zv74cQ7kNvgGsUtb2&_nc_zt=23&_nc_ht=scontent.ftun14-1.fna&_nc_gid=ANmLEEWqPw9zFeNMgO63QOh&oh=00_AYC65UgCQtdbIf50zYdCL-GWNhNInPTzBcy5exm_bAZ7pg&oe=67982D3B",
+      links: [
+        {
+          title: "Github",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Smartshield-INSAT",
+        },
+      ],
+    },
+    {
+      title: "2nd Place 🥈 Artificial Intelligence National Summit (AINS 2.0)",
+      dates: "June 9, 2024",
+      location: "Sousse, Tunisia",
+      description:
+        "Created a developer tool for machine learning professionals, a Multi-LLM Agent System, for problem definition, data analysis, model recommendations, research, and code generation.",
+      image:
+        "https://scontent.ftun14-1.fna.fbcdn.net/v/t39.30808-6/417397315_814527174044531_2047266368184133502_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=5iiZ2iTHJ9YQ7kNvgEmkpJa&_nc_zt=23&_nc_ht=scontent.ftun14-1.fna&_nc_gid=Ao9HDJK1llokEZGQDwjQASn&oh=00_AYC9xaCYbbkAl3-I-Amfs26pB3d_Ig4Q7dijrIFWCw6gbw&oe=6798214E",
+      links: [
+        {
+          title: "Github",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/KacemMathlouthi/AINS-ML.Guide",
+        },
+      ],
+    },
+    {
+      title: "4th Place ✨ Orange Summer Challenge (OSC 2024)",
       dates: "October 13th - 15th, 2017",
-      location: "Waterloo, Ontario",
+      location: "Tunis, Tunisia",
       description:
-        "Developed a blockchain application for doctors and pharmacists to perform trustless transactions and prevent overdosage in patients.",
+        "Developed innovative AI solutions for agriculture, gaining teamwork and problem-solving skills.",
       image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/eth-waterloo.png",
-      links: [
-        {
-          title: "Organization",
-          icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/ethdocnet",
-        },
-      ],
+        "https://scontent.ftun14-1.fna.fbcdn.net/v/t39.30808-6/467487014_857470296551848_4425247407313207464_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=7RdsDErnEoEQ7kNvgGGKlvu&_nc_zt=23&_nc_ht=scontent.ftun14-1.fna&_nc_gid=ATzSzeW4OkQueBUBEYwN_3e&oh=00_AYClhDxncDXzsXKdBiseSN80jDS_3K_1PbAuGLyzxm_9zA&oe=679838FA",
+      links: [],
     },
     {
-      title: "Hack The North",
-      dates: "September 15th - 17th, 2017",
-      location: "Waterloo, Ontario",
+      title: "3rd Place 🥉 CodeQuest",
+      dates: "December 13, 2023",
+      location: "Tunis, Tunisia",
       description:
-        "Developed a virtual reality application allowing users to see themselves in third person.",
+        "ACM INSAT Competitive Programming competition, honed problem-solving and teamwork skills.",
       image:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-the-north.png",
-      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
-      links: [
-        {
-          title: "Streamer Source",
-          icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/justinmichaud/htn2017",
-        },
-        {
-          title: "Client Source",
-          icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/dillionverma/RTSPClient",
-        },
-      ],
+        "https://scontent.ftun14-1.fna.fbcdn.net/v/t39.30808-1/402598779_6955319747862871_2681866326282106923_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=JlgCKg75t9MQ7kNvgHtPmKJ&_nc_zt=24&_nc_ht=scontent.ftun14-1.fna&_nc_gid=AbaRmP2Tb28KlD7nCvNv_eJ&oh=00_AYBoQb-cgAkp1HiEFvrIntQiQUoHsPXGFICgAkugF0Gl0g&oe=67984D42",
+      links: [],
     },
   ],
 } as const;
